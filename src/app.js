69596cea536c5b67800abfabe9faa583cbd60756
Antonybsb <artbsb1@gmail.com>
2025-03-09 11:40:45 -0300

    Bug loop infinito

--- src/app.js
+++ src/app.js
@@ -1,13 +1,14 @@
 const express = require('express');
+const routes = require('./routes');
 
 const app = express();
+routes(app);
 
-app.use(express.json());
-
-app.get('/teste', (req, res) => {
-  res
-    .status(200)
-    .send({ mensagem: 'boas-vindas à API' });
-});
+// app.use(express.json());
+// app.get('/teste', (req, res) => {
+//   res
+//     .status(200)
+//     .send({ mensagem: 'boas-vindas à API' });
+// });
 
 module.exports = app;
